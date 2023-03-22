b36633953905cd761791cdb6c0fbf395bff68c37
YuriTech82 <shinjungtee@gmail.com>
2023-03-22 19:13:27 +0300

    All Tasks

---
+++ exec.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+/**
+ * execute - executes the opcode
+ * @stack: points to the head of the stack
+ * @file: points to the monty file
+ * @line_num: line number in the monty file
+ * @content: content of the line number
+ */
+void execute(stack_t **stack, FILE *file, unsigned int line_num, char *content)
+{
+	instruction_t op_ins[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pop", pop},
+		{"pint", pint},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", _div},
+		{"mul", mul},
+		{"mod", mod},
+		{NULL, NULL}
+	};
+	char *op;
+	unsigned int i = 0;
+
+	op = strtok(content, " \n\t");
+
+	if (op != NULL && op[0] == '#')
+		return;
+	arg = strtok(NULL, " \n\t");
+
+	while (op_ins[i].opcode != NULL && op != NULL)
+	{
+		if (strcmp(op_ins[i].opcode, op) == 0)
+		{
+			op_ins[i].f(stack, line_num);
+			return;
+		}
+		i++;
+	}
+	if (op_ins[i].opcode == NULL)
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_num, op);
+		fclose(file);
+		free(content);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+}
